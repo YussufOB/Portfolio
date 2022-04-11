bf3fb69d29154b6460169177c06218f6fec1f3db
YussufOB <hamzyuss1@gmail.com>
2022-04-11 22:09:20 +0000

    hamburger feature reviewed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// HAMBURGER SCRIPT
+
+const pagewrapper = document.querySelector('.wrapper');
 const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
@@ -8,22 +11,27 @@ const shieldOff3 = document.querySelector('.shieldOff3');
 function show() {
   mainMenu.style.display = 'flex';
   mainMenu.style.top = '0';
+  pagewrapper.style.filter = 'blur(4px)';
 }
 
 function hide() {
-  mainMenu.style.top = '-100%';
+  mainMenu.style.display = 'none';
+  pagewrapper.style.filter = 'blur(0)';
 }
 
 function shield1() {
   mainMenu.style.display = 'none';
+  pagewrapper.style.filter = 'blur(0)';
 }
 
 function shield2() {
   mainMenu.style.display = 'none';
+  pagewrapper.style.filter = 'blur(0)';
 }
 
 function shield3() {
   mainMenu.style.display = 'none';
+  pagewrapper.style.filter = 'blur(0)';
 }
 
 openMenu.addEventListener('click', show);
